0fc49290ac5063b83217a76bd58fee8c09e8461d
EmilJuntikka <emiljuntikka@gmail.com>
2025-08-19 14:26:30 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,15 +6,10 @@ import {getFirestore} from "@firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4wiiL1XZ__Ak8-6fOHPd_6B6LD_h8-18",
-  authDomain: "quiz-app-898b9.firebaseapp.com",
-  databaseURL: "https://quiz-app-898b9-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "quiz-app-898b9",
-  storageBucket: "quiz-app-898b9.firebasestorage.app",
-  messagingSenderId: "520315406850",
-  appId: "1:520315406850:web:c47ec05c248a172fb0b51b"
+This is where I put my secret codes
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
